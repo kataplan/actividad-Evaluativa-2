da999201fa014d69c61678a67aea5955460ddceb
kataplan <dcatalanmarchese@gmail.com>
2021-04-06 12:26:31 -0400

    final

--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,49 @@
 "use strict";
-var nombre;
-var edad;
+var form = document.getElementById("formulario");
+var box = document.getElementsByClassName("box");
+form.addEventListener("submit", function (evento) {
+    var nombre = form.elements.namedItem("nombre");
+    var apellido = form.elements.namedItem("apellido");
+    var rut = form.elements.namedItem("rut");
+    var email = form.elements.namedItem("email");
+    var phoneNumber = form.elements.namedItem("number");
+    var programas = form.elements.namedItem("programas");
+    var rango = form.elements.namedItem("rango");
+    var experiencia = form.elements.namedItem("programas");
+    var programasSeleccionados = [];
+    var inputElements = [nombre, apellido, rut, email, phoneNumber];
+    evento.preventDefault();
+    var verification = true;
+    //programas
+    programas.forEach(function (programa) {
+        var checkbox = programa;
+        if (checkbox.checked) {
+            programasSeleccionados.push(checkbox.value);
+        }
+    });
+    //experencia
+    if (programasSeleccionados.length == 0) {
+        verification = false;
+    }
+    inputElements.forEach(function (inputElements) {
+        console.log(inputElements.value);
+        if (inputElements.value.length == 0) {
+            verification = false;
+            return false;
+        }
+    });
+    if (verification) {
+        form.style.display = "none";
+        box[0].style.display = "none";
+        var mensaje = document.getElementById("mensaje");
+        mensaje.style.display = "block";
+    }
+    else {
+        alert("Ingrese los campos corectamente");
+    }
+});
+function limpiarDatos() {
+    form.reset();
+}
+var limpiar = document.getElementById("btnLimpiar");
+limpiar.addEventListener("click", limpiarDatos);
--- ts/inicio.ts
+++ ts/inicio.ts
@@ -1,2 +1,64 @@
-let nombre:string;
-let edad:number;
+let form=<HTMLFormElement>document.getElementById("formulario");
+let box:any=document.getElementsByClassName("box");
+
+
+form.addEventListener("submit",function(evento:any){
+    let nombre = <HTMLInputElement>form.elements.namedItem("nombre");
+    let apellido= <HTMLInputElement>form.elements.namedItem("apellido");
+    let rut= <HTMLInputElement>form.elements.namedItem("rut");
+    let email= <HTMLInputElement>form.elements.namedItem("email");
+    let phoneNumber= <HTMLInputElement>form.elements.namedItem("number");
+    let programas =  <RadioNodeList>form.elements.namedItem("programas");
+    let rango= <HTMLInputElement> form.elements.namedItem("rango");
+    let experiencia=<RadioNodeList>form.elements.namedItem("programas");
+    let programasSeleccionados:String[] =[];
+    
+
+    let inputElements:Array <HTMLInputElement> = [nombre,apellido,rut,email,phoneNumber];
+    evento.preventDefault();
+    let verification:boolean =true
+    
+    //programas
+    programas.forEach((programa: Node)=>{
+        const checkbox = <HTMLInputElement>programa;
+        if(checkbox.checked){
+            programasSeleccionados.push(checkbox.value);
+        }
+    })
+    //experencia
+    
+   
+    if(programasSeleccionados.length== 0 ){
+        verification=false;
+    }
+
+    inputElements.forEach(function(inputElements){
+        console.log(inputElements.value)
+        if(inputElements.value.length == 0){
+            verification= false;
+            return false;
+            
+        }
+    })
+    
+
+    if(verification){
+        form.style.display="none";
+        box[0].style.display="none";
+        let mensaje:any = document.getElementById("mensaje");
+        mensaje.style.display="block";
+       
+    }else{
+        alert("Ingrese los campos corectamente");}
+    
+    
+});
+
+
+function  limpiarDatos(){
+    form.reset();
+}
+
+
+let limpiar:any = <HTMLButtonElement> document.getElementById("btnLimpiar")
+limpiar.addEventListener("click", limpiarDatos);
